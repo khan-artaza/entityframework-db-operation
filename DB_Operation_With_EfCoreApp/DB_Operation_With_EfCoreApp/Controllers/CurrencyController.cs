d1787324127f94201888b0edd26fb5fdba7706f0
Artaza <artazakhan556@gmail.com>
2026-06-21 12:17:37 +0530

    added api controller named currencycontroller to fetch currency data from db when any GET request are coming on /api/currencies

---
+++ DB_Operation_With_EfCoreApp/DB_Operation_With_EfCoreApp/Controllers/CurrencyController.cs
@@ -0,0 +1,44 @@
+﻿// We need AppDbContext because it contains our database connection
+using DB_Operation_With_EfCoreApp.Data;
+
+// Basic ASP.NET Core classes
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+// Namespace is like a folder for related classes
+namespace DB_Operation_With_EfCoreApp.Controllers
+{
+    // All requests starting with: "api/currencies" will come to this controller
+    [Route("api/currencies")]
+    [ApiController]// Tells ASP.NET that this is a Web API controller
+    public class CurrencyController : ControllerBase // ControllerBase gives us methods like: Ok(), NotFound(), BadRequest() etc.
+    {
+        private readonly AppDbContext _dbcontext; // Variable to store database connection, readonly means once assigned, it cannot be changed
+
+        // Constructor
+        // ASP.NET automatically gives us AppDbContext object
+        // through Dependency Injection (DI)
+        public CurrencyController(AppDbContext dbcontext)
+        {
+            // Store the received database object
+            // inside our private variable
+            _dbcontext = dbcontext;
+        }
+
+        // This method handles GET requests
+        // URL: GET /api/currencies
+        [HttpGet("")]
+
+        // IActionResult means this method returns an HTTP response (200, 404, 400, etc.)
+        public IActionResult GetAllCurrencies()
+        {
+            // Go to CurrencyTypes table
+            // Fetch all records
+            // Convert them into a List
+            var result = _dbcontext.CurrencyTypes.ToList();
+
+            // Return status code 200 (OK) along with the data as JSON
+            return Ok(result);
+        }
+    }
+}
