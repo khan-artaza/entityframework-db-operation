fe1056f58af3df44af23595a35b627ad5a95827e
Artaza <artazakhan556@gmail.com>
2026-06-21 11:32:38 +0530

    seeding value for master table(currencyType and Language)

--- DB_Operation_With_EfCoreApp/DB_Operation_With_EfCoreApp/Data/AppDbContext.cs
+++ DB_Operation_With_EfCoreApp/DB_Operation_With_EfCoreApp/Data/AppDbContext.cs
@@ -9,6 +9,23 @@ namespace DB_Operation_With_EfCoreApp.Data
         {
         }
 
+        //overriding the OnModelCreating method to configure the model using Fluent API
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CurrencyType>().HasData(
+                new CurrencyType { Id = 1, Currency = "USD", Description = "US Dollar" },
+                new CurrencyType { Id = 2, Currency = "EUR", Description = "Euro" },
+                new CurrencyType { Id = 3, Currency = "GBP", Description = "British Pound" }
+                );//seeding the CurrencyType table with initial data using the HasData method
+
+            modelBuilder.Entity<Language>().HasData(
+                new Language { Id = 1, Title = "English", Description = "English Language" },
+                new Language { Id = 2, Title = "Spanish", Description = "Spanish Language" },
+                new Language { Id = 3, Title = "Hindi", Description = "Hindi Language" },
+                new Language { Id = 4, Title = "Urdu", Description = "Urdu Language" }
+                );//seeding the Language table with initial data using the HasData method
+        }
+
         public DbSet<Book> Books { get; set; } //creating a DbSet property for the Book entity to represent the Books table in the database
 
         public DbSet<Language> Languages { get; set; } //creating a DbSet property for the Language entity to represent the Languages table in the database
---
+++ DB_Operation_With_EfCoreApp/DB_Operation_With_EfCoreApp/Migrations/20260621055805_seeding_value_for_lang_curr.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace DB_Operation_With_EfCoreApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class seeding_value_for_lang_curr : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "CurrencyTypes",
+                columns: new[] { "Id", "Currency", "Description" },
+                values: new object[,]
+                {
+                    { 1, "USD", "US Dollar" },
+                    { 2, "EUR", "Euro" },
+                    { 3, "GBP", "British Pound" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Languages",
+                columns: new[] { "Id", "Description", "Title" },
+                values: new object[,]
+                {
+                    { 1, "English Language", "English" },
+                    { 2, "Spanish Language", "Spanish" },
+                    { 3, "Hindi Language", "Hindi" },
+                    { 4, "Urdu Language", "Urdu" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "CurrencyTypes",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "CurrencyTypes",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "CurrencyTypes",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Languages",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Languages",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Languages",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Languages",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
