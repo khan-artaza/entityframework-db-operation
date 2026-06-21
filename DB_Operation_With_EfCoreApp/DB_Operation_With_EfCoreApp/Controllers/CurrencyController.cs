a350f6cd0c625ebb2126baaf38f5e0836229de3e
Artaza <artazakhan556@gmail.com>
2026-06-21 12:48:41 +0530

    following the async approach to fetching data from db in our api controller and all another language controller

--- DB_Operation_With_EfCoreApp/DB_Operation_With_EfCoreApp/Controllers/CurrencyController.cs
+++ DB_Operation_With_EfCoreApp/DB_Operation_With_EfCoreApp/Controllers/CurrencyController.cs
@@ -4,6 +4,7 @@ using DB_Operation_With_EfCoreApp.Data;
 // Basic ASP.NET Core classes
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 // Namespace is like a folder for related classes
 namespace DB_Operation_With_EfCoreApp.Controllers
@@ -14,11 +15,7 @@ namespace DB_Operation_With_EfCoreApp.Controllers
     public class CurrencyController : ControllerBase // ControllerBase gives us methods like: Ok(), NotFound(), BadRequest() etc.
     {
         private readonly AppDbContext _dbcontext; // Variable to store database connection, readonly means once assigned, it cannot be changed
-
-        // Constructor
-        // ASP.NET automatically gives us AppDbContext object
-        // through Dependency Injection (DI)
-        public CurrencyController(AppDbContext dbcontext)
+        public CurrencyController(AppDbContext dbcontext)  // Constructor: ASP.NET automatically gives us AppDbContext object through Dependency Injection (DI)
         {
             // Store the received database object
             // inside our private variable
@@ -30,12 +27,13 @@ namespace DB_Operation_With_EfCoreApp.Controllers
         [HttpGet("")]
 
         // IActionResult means this method returns an HTTP response (200, 404, 400, etc.)
-        public IActionResult GetAllCurrencies()
+        public async Task<IActionResult> GetAllCurrencies()
         {
-            // Go to CurrencyTypes table
-            // Fetch all records
-            // Convert them into a List
-            var result = _dbcontext.CurrencyTypes.ToList();
+            //var result = await _dbcontext.CurrencyTypes.ToListAsync(); //Go to CurrencyTypes table, Fetch all records, Convert them into a List
+
+            //another way
+            var result = await (from currencies in _dbcontext.CurrencyTypes
+                         select currencies).ToListAsync(); 
 
             // Return status code 200 (OK) along with the data as JSON
             return Ok(result);
---
+++ DB_Operation_With_EfCoreApp/DB_Operation_With_EfCoreApp/Controllers/LanguageController.cs
@@ -0,0 +1,31 @@
+﻿using DB_Operation_With_EfCoreApp.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DB_Operation_With_EfCoreApp.Controllers
+{
+    [Route("api/languages")]
+    [ApiController]
+    public class LanguageController : ControllerBase
+    {
+        private readonly AppDbContext _dbcontext;
+
+        public LanguageController(AppDbContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+
+        [HttpGet("")]
+        public async Task<IActionResult> GetAllLanguage()
+        {
+            var result = await _dbcontext.Languages.ToListAsync();
+
+            //another way
+            //var result = await (from languages in _dbcontext.Languages
+                               // select languages).ToListAsync();
+
+            return Ok(result);
+        }
+    }
+}
